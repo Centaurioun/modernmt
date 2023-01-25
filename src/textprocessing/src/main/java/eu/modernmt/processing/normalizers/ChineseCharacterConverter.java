0a9622529f2d4a2769bbc392394eecc950aad785
centaurioun <yiseri@anadolu.edu.tr>
2023-01-26 02:58:36 +0300

    update

--- src/textprocessing/src/main/java/eu/modernmt/processing/normalizers/ChineseCharacterConverter.java
+++ src/textprocessing/src/main/java/eu/modernmt/processing/normalizers/ChineseCharacterConverter.java
@@ -1,5 +1,4 @@
 package eu.modernmt.processing.normalizers;
-
 import eu.modernmt.lang.Language;
 import eu.modernmt.lang.UnsupportedLanguageException;
 import eu.modernmt.model.Translation;
@@ -16,30 +15,9 @@ import java.util.Map;
 
 public class ChineseCharacterConverter extends TextProcessor<Translation, Translation> {
 
-    private static Map<Integer, Integer> loadDictionary(String filename) {
-        HashMap<Integer, Integer> result = new HashMap<>();
-
-        InputStream stream = null;
-        LineIterator iterator = null;
-
-        try {
-            stream = ChineseCharacterConverter.class.getResourceAsStream(filename);
-            iterator = IOUtils.lineIterator(stream, "UTF-8");
-            while (iterator.hasNext()) {
-                String line = iterator.nextLine();
-                String[] keyValues = line.split("\t", 2);
-                Integer key = keyValues[0].codePointAt(0);
-                Integer value = keyValues[1].codePointAt(0);
-                result.put(key, value);
-            }
-
-            return result;
-        } catch (IOException e) {
-            throw new Error(e);
-        } finally {
-            IOUtils.closeQuietly(stream);
-            if (iterator != null)
-                iterator.close();
+    private static class DictionaryLoadException extends ProcessingException {
+        public DictionaryLoadException(String message) {
+            super(message);
         }
     }
 
@@ -47,13 +25,10 @@ public class ChineseCharacterConverter extends TextProcessor<Translation, Transl
 
     public ChineseCharacterConverter(Language sourceLanguage, Language targetLanguage) throws UnsupportedLanguageException {
         super(sourceLanguage, targetLanguage);
-
         String language = targetLanguage.getLanguage();
         String region = targetLanguage.getRegion();
-
         if (!"zh".equals(language))
             throw new UnsupportedLanguageException(targetLanguage);
-
         if (region == null)
             chars = null;
         else if ("CN".equals(region))
@@ -73,14 +48,12 @@ public class ChineseCharacterConverter extends TextProcessor<Translation, Transl
                     word.setText(text);
             }
         }
-
         return param;
     }
 
     private String convert(String text) {
         if (text.codePoints().noneMatch(chars::containsKey))
             return null;
-
         StringBuilder result = new StringBuilder(text.length());
         text.codePoints().forEach(code -> {
             Integer conversion = chars.get(code);
@@ -90,8 +63,30 @@ public class ChineseCharacterConverter extends TextProcessor<Translation, Transl
                 result.appendCodePoint(conversion);
             }
         });
-
         return result.toString();
     }
 
+    private static Map<Integer, Integer> loadDictionary(String filename) {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        InputStream stream = null;
+        LineIterator iterator = null;
+        try {
+            stream = ChineseCharacterConverter.class.getResourceAsStream(filename);
+            iterator = IOUtils.lineIterator(stream, "UTF-8");
+            while (iterator.hasNext()) {
+                String line = iterator.nextLine();
+                String[] keyValues = line.split("\t", 2);
+                Integer key = keyValues[0].codePointAt(0);
+                Integer value = keyValues[1].codePointAt(0);
+                result.put(key, value);
+            }
+            return result;
+        } catch (IOException e) {
+            throw new DictionaryLoadException("Error loading dictionary: " + filename);
+        } finally {
+            IOUtils.closeQuietly(stream);
+            if (iterator != null)
+                iterator.close();
+        }
+    }
 }
